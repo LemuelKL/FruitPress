75c6b3b054dacf11be6b08ba66963f73af9ce359
LemuelKL <lemuellee.kl@gmail.com>
2022-08-13 02:36:05 +0800

    wrap align for orphan newlines

--- pandoc/Filter.hs
+++ pandoc/Filter.hs
@@ -2,6 +2,7 @@
 -- Filter.hs
 import Text.Pandoc.JSON
 import Data.Text
+import Data.List
 
 main :: IO ()
 main = toJSONFilter press
@@ -11,7 +12,7 @@ press (Span (_,x:_,_) l) | x == pack "math-tex" = convert l
 press x = x
 
 convert :: [Inline] -> Inline
-convert l = Math InlineMath (Data.Text.dropEnd 2 (Data.Text.drop 2 (fixdegree (myconcat l)))) 
+convert l = Math InlineMath (wrapalign ((Data.Text.dropEnd 2 (Data.Text.drop 2 (katex2latex (myconcat l))))))
 
 myconcat :: [Inline] -> Text
 myconcat l = (pack (Prelude.foldl (\ y (Str x) -> y++(unpack x)) "" (Prelude.map stringify l)))
@@ -19,29 +20,21 @@ myconcat l = (pack (Prelude.foldl (\ y (Str x) -> y++(unpack x)) "" (Prelude.map
 stringify :: Inline -> Inline
 stringify x@(Str _) = x
 stringify Space = Str (pack " ")
+stringify x = Str (pack " ")
+
+katex2latex :: Text -> Text
+katex2latex = fixweirdcharacters . fixnotequal . fixdegree
+
+fixnotequal :: Text -> Text
+fixnotequal = replace (pack "\\cancel=") (pack "\\neq")  . replace (pack "\\cancel{=}") (pack "\\neq")
+
+-- zero width space
+fixweirdcharacters :: Text -> Text
+fixweirdcharacters = replace (pack "\x200B") (pack " ")
 
 fixdegree :: Text -> Text
-fixdegree x = replace (pack "\\degree") (pack "°") x
+fixdegree = replace (pack "\\degree") (pack "^\\circ")
 
--- EXAMPLE INPUT PANDOC AST
--- 
--- Pandoc
---   Meta { unMeta = fromList [] }
---   [ Para
---       [ Span
---           ( "" , [ "math-tex" ] , [] )
---           [ Str "\\(\\alpha^2+*c\\alpha+*d\\beta^2+*e\\beta\\)" ]
---       ]
---   , BulletList
---       [ [ Plain
---             [ Span
---                 ( "" , [ "math-tex" ] , [] )
---                 [ Str
---                     "\\(\\left(\\alpha+*f\\beta\\right)\\left(\\alpha+*g\\beta+*h\\right)\\)"
---                 ]
---             ]
---         ]
---       ]
---   , Para [ Str "\160" ]
---   ]
--- 
+-- wrap align
+wrapalign :: Text -> Text
+wrapalign x = if (Data.Text.isInfixOf (pack "\\begin{aligned}") x) then x else (pack("\\begin{aligned}") <> x <> pack("\\end{aligned}"))
